931bf24ddd9bd7dab7a8b2c126f373c4866b720d
Joao Novais <150026595+jpcnovais@users.noreply.github.com>
2024-05-13 19:31:44 +0100

    initial commit

---
+++ AAM/Assets/Mena-xr.com/360 Tour/Scripts/Navigater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigater : MonoBehaviour
+{
+    public int index = 0;
+
+    void OnMouseUp()
+    {
+        index = (index + 1) % ViewerManager.Instance.views.Count; // Avança para o próximo índice circularmente
+        ViewerManager.Instance.LoadNextView(index);
+    }
+
+    void OnMouseEnter()
+    {
+        CursorManager.Instance.SetCursor(CursorType.Hover);
+    }
+
+    void OnMouseExit()
+    {
+        CursorManager.Instance.SetCursor(CursorType.Normal);
+    }
+}
